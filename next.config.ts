91410db22faea2d3b2579477dbf2f8c6fec650a1
681group <office@681.group>
2026-01-10 17:28:24 +0000

    Next.js Update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: 'export',
+   // Required for static export on GitHub Pages if you use the Next.js Image component
+   images: {
+     unoptimized: true,
+   },
+   // If your repo is https://github.com/User/RepoName, uncomment the line below:
+   // basePath: '/RepoName',
    devIndicators: false,
    reactStrictMode: true,
 
@@ -10,4 +16,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
